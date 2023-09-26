1627402846d2d10a428cd09158ac3d9b48777f00
WoitkeL <linus.woitke@uni-jena.de>
2023-09-26 16:54:16 +0200

    Update and rename database_exe.py to make_biomodels_csv.py

--- code/make_biomodels_csv.py
+++ code/make_biomodels_csv.py
@@ -7,8 +7,5 @@ Created on Thu Jan 12 20:48:14 2023
 
 from iterate_over_DB import iterate_over_database
 if __name__ == '__main__':
-    safe_object=[]
-    database_dict={}
-    iterate_over_database(path="C:/Users/linus/python/BioModels-files/",exelname="biomodels_paper")
+    iterate_over_database(path="C:/Users/linus/python/BioModels-files/",exelname="biomodels")
 
-    print(safe_object)
